7447becf2765111a83b8cc8abdce2b3662a59f2c
WeberDS <miguel_weber@estudante.sesisenai.org.br>
2026-09-17 09:15:18 -0300

    codigo solicita dois numeros e exibe

---
+++ parte1-variaveis/exercicio2_.py
@@ -0,0 +1,8 @@
+print ("digite o primeiro numero")
+numero1 = input ()
+print ("Digite o segundo numero")
+numero2 = input ()
+print ("o primeiro numero é: ",numero1)
+print ("o segundo numero é: ",numero2)
+
+
