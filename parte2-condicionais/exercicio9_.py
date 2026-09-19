9cd63cc38db649a3e830c514e830abe329699581
WeberDS <miguel_weber@estudante.sesisenai.org.br>
2026-09-19 13:48:42 -0300

    adiciona código para avaliar a nota do aluno e informar sua situação acadêmica

---
+++ parte2-condicionais/exercicio9_.py
@@ -0,0 +1,7 @@
+nota = float(input("Digite a nota do aluno: "))
+if nota >= 7:
+    print("O aluno está aprovado")
+elif nota >= 5:
+    print("O aluno está em recuperação")
+else:
+    print("O aluno está reprovado")
