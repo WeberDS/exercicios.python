0cd0898d675de4a7085508f7ee6c0772d21853d0
WeberDS <miguel_weber@estudante.sesisenai.org.br>
2026-09-19 11:29:47 -0300

    O código solicita o valor de um produto e sua quantidade, após isso calcula o valor total.

---
+++ parte1-variaveis/exercicio5_.py
@@ -0,0 +1,4 @@
+valor = float(input("Digite o valor do produto em R$:  "))
+quantidade = float(input("Digite a quantidade do produto: "))
+valorFinal = valor * quantidade
+print (f"O valor total foi de: R$ {valorFinal:.2f}")
